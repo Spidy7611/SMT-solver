aab09ba2c8750d101933d6f5b93c46f063095035
Foltán Bálint <foltika56@gmail.com>
2026-09-20 13:38:04 +0200

    Update Parser.cpp

--- parser/Parser.cpp
+++ parser/Parser.cpp
@@ -51,20 +51,7 @@ Formula parseCNF(const std::string& filename) {
 }
 
 int parseSMTLIB(const std::string& filename) {
-    //symbol_table["func_name"] = body_term_id; ilyesmi function to term mapping
-    //OPok enumba
-    /*enum class OpKind { 
-    VAR,    
-    CONST,  
-    BV_ADD, 
-    BV_AND  
-};
-pl
-*/
-/*
-vami struct ami op kindot és term idt tartalmaz
-*/
-//az összes term egy hatvány halmaz?
+
 
 }
   
