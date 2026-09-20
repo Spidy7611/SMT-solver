a7a7196a1d117f8dda3bc16a20822dcdd56b2be2
Foltán Bálint <foltika56@gmail.com>
2026-09-20 13:37:58 +0200

    Create SMTtypes.h

---
+++ types/SMTtypes.h
@@ -0,0 +1,14 @@
+    //symbol_table["func_name"] = body_term_id; ilyesmi function to term mapping
+    //OPok enumba
+    /*enum class OpKind { 
+    VAR,    
+    CONST,  
+    BV_ADD, 
+    BV_AND  
+};
+pl
+*/
+/*
+vami struct ami op kindot és term idt tartalmaz
+*/
+//az összes term egy hatvány halmaz?
